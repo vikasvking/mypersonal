39419835f7f317eb15ef61663ddb9f809594ab39
vikasvking <vikas.vk711@gmail.com>
2018-11-17 20:04:16 +0530

    aded

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,16 @@
 module HomeHelper
+	def taskcounter i
+		c=0
+		z=0
+		Currentaffair.all.each do |ca|
+			if ca.todo==i.name && ca.done==true
+				c+=1
+			end
+			if ca.todo==i.name && ca.done!=true
+				z+=1
+			end
+		end
+
+		return [c,z]
+	end
 end
